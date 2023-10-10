d68fa44207f7bdc0ba68d218dab65c589baca44b
BrianEtemesi <etemecci@gmail.com>
2023-10-10 16:25:37 +0000

    call app instance

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 """
-simple flask application to display hello world
+A simple Flask application to display "Hello world".
 """
 
+# Import the Flask module
 from flask import Flask
 
+# Create an instance of the Flask application
 app = Flask(__name__)
 
+# Define a route for the root URL ("/")
 @app.route("/")
 def hello_world():
     """
-    show hello world text
+    Show the "Hello world" text when the root URL is accessed.
     """
     return "Hello world"
+
+# Entry point for the application
+if __name__ == '__main__':
+    # Run the Flask app in debug mode, which provides better error messages
+    app.run(debug=True)
